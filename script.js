d649e1ebf9fb2bf499de1e55207eb1a79ad9c576
leticea <nascimento.leticeah@gmail.com>
2023-01-11 19:25:29 -0300

    codando js

--- script.js
+++ script.js
@@ -24,8 +24,28 @@ function initBattery() {
       }
 
       if (level <= 20) {
-        
+        batteryLiquid.classList.add("gradient-color-red");
+        batteryLiquid.classList.remove("gradient-color-green", "gradient-color-orange", "gradient-color-yellow");
+
+      } else if (level <= 48) {
+        batteryLiquid.classList.add("gradient-color-orange");
+        batteryLiquid.classList.remove("gradient-color-green", "gradient-color-red", "gradient-color-yellow");
+
+      } else if (level <= 80) {
+        batteryLiquid.classList.add("gradient-color-yellow");
+        batteryLiquid.classList.remove("gradient-color-green", "gradient-color-orange", "gradient-color-red");
+
+      } else {
+        batteryLiquid.classList.add("gradient-color-green");
+        batteryLiquid.classList.remove("gradient-color-red", "gradient-color-orange", "gradient-color-yellow");
       }
     }
-  })
+    updateBattery();
+    batt.addEventListener("chargingchange", () => {
+      updateBattery()
+    });
+    batt.addEventListener("levelchange", () => {
+      updateBattery
+    });
+  });
 }

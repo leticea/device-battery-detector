cc46b38aa9d01cce26bcfb690791361f108b1910
leticea <nascimento.leticeah@gmail.com>
2023-01-11 19:18:46 -0300

    codando js

--- script.js
+++ script.js
@@ -11,6 +11,19 @@ function initBattery() {
       batteryLiquid.style.height = `${parseInt(batt.level * 100)}%`;
       if (level == 100) {
         batteryStatus.innerHTML = `Battery Full <i class="ri-battery-2-fill green-color"></i>`;
+        batteryLiquid.style.height = "103%";
+
+      } else if (level <= 20 &! batt.charging) {
+        batteryStatus.innerHTML = `Low Charge <i class="ri-plug-line animated-red animated-red"></i>`;
+
+      } else if (batt.charging) {
+        batteryStatus.innerHTML = `Charging... <i class="ri-flashlight-line animated-green"></i>`;
+
+      } else {
+        batteryStatus.innerHTML = "";
+      }
+
+      if (level <= 20) {
         
       }
     }
